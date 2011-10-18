9a29210c05e729a70935ea5197e118f1a1485b57
Mike Heffner <mikeh@fesnel.com>
2011-10-18 19:55:36 -0400

    Add recipe define.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ description      "Installs/Configures ec2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
+recipe "ec2::raid_ephemeral", "Creates a RAID0 on the ephemeral EC2 drives."
+
 # TODO: test on fedora
 #
 %w{ubuntu}.each do |os|
