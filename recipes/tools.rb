2cf9575c8e951d4af896b07b2e420e56ca32072f
Mike Heffner <mikeh@fesnel.com>
2011-10-24 18:10:52 -0400

    Fix previous update.

--- recipes/tools.rb
+++ recipes/tools.rb
@@ -5,8 +5,8 @@
 template "/etc/apt/sources.list.d/multiverse.list" do
   source "multiverse.sources.erb"
   mode "0644"
-  variables :version => %{lsb_release -cs}.chomp
-  notifies :run, "execute[apt-get -y update]", :immediately
+  variables :version => %x{lsb_release -cs}.chomp
+  notifies :run, "execute[apt-get update]", :immediately
 end
 
 package "ec2-api-tools"
