b342ea6b716840ee276a362dafef2d3ad401f3a4
Mike Heffner <mikeh@fesnel.com>
2012-11-13 20:36:10 -0500

    AMI tools version was bumped

--- attributes/ec2.rb
+++ attributes/ec2.rb
@@ -7,7 +7,7 @@ default[:ec2][:raid_mount] = "/raid0"
 
 # EC2 AMI tools
 
-default[:ec2][:ami_tools_version] = "1.4.0.7"
+default[:ec2][:ami_tools_version] = "1.4.0.9"
 default[:ec2][:ami_tools_url] = "http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip"
-default[:ec2][:ami_tools_sha] = "376204b297d104a07c0011e81f98a3f91457a46c4c0833135c60f01087300c51"
+default[:ec2][:ami_tools_sha] = "9876de865e55053578d5d716d4325166262f7ee9e965e2987103b21b45969745"
 default[:ec2][:ami_tools_install_dir] = "/opt"
